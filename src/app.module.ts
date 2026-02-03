89cf225a098988ec91d1c46437581f79dc69ed66
Niakrist <ak_88dron@inbox.ru>
2026-02-03 12:37:25 +0300

    feat emplement category

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot(), UserModule],
+  imports: [AuthModule, ConfigModule.forRoot(), UserModule, CategoryModule],
   controllers: [],
   providers: [],
 })
